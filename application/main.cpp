4492cc506d1e1a37d7c284ef190bbbf45c64b359
vinnoplexus <vinay.sable@innoplexus.com>
2017-05-10 08:44:45 +0530

    added mongocxx code and command to execute the application g++ --std=c++11  main.cpp HTTPDownloader.cpp -o http -I/usr/local/include/libmongoc-1.0  -I/usr/local/include/libbson-1.0  -I/usr/local/include/bsoncxx/v_noabi -I/usr/local/include/mongocxx/v_noabi -L/usr/local/lib -lmongocxx -lbsoncxx -lcurl -Wl,-rpath /usr/local/lib

--- application/main.cpp
+++ application/main.cpp
@@ -1,11 +1,38 @@
 #include "HTTPDownloader.h"
 #include <iostream>
-#include <string>
-using namespace std;
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+
+using bsoncxx::builder::stream::close_array;
+using bsoncxx::builder::stream::close_document;
+using bsoncxx::builder::stream::document;
+using bsoncxx::builder::stream::finalize;
+using bsoncxx::builder::stream::open_array;
+using bsoncxx::builder::stream::open_document;
+
+
+
 
 int main(int argc, char** argv) {
 
     HTTPDownloader downloader;
     std::string content = downloader.download("https://stackoverflow.com");
-    cout << content << std::endl;
+//    cout << content << std::endl;
+
+    mongocxx::instance inst{};
+    mongocxx::client conn{mongocxx::uri{}};
+
+    bsoncxx::builder::stream::document document{};
+
+    auto collection = conn["test"]["testcollection"];
+    document << "hello" << "world";
+
+    collection.insert_one(document.view());
+    auto cursor = collection.find({});
+
+    for (auto&& doc : cursor) {
+        std::cout << bsoncxx::to_json(doc) << std::endl;
+    }
 }
